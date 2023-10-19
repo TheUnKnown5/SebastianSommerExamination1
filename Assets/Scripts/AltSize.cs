945db1c389e21ef34ea1a3cd641b22279a9170c0
Sebastian Sommer <sebastian.sommer@learnet.se>
2023-10-19 16:32:33 +0200

    Update

--- Assets/Scripts/AltSize.cs
+++ Assets/Scripts/AltSize.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class AltSize : MonoBehaviour
 {
-    [SerializeField] float maxVal = 1.5f;
-    [SerializeField] float minVal = -1.5f;
     void Update()
     {
         transform.localScale = transform.localScale + new Vector3(Random.Range(-0.002f, 0.002f), Random.Range(-0.002f, 0.002f), 0);
-        //transform.localScale = new Vector2(Mathf.Clamp(Random.Range(-0.002f, 0.002f), minVal, maxVal), 0);
-        //transform.localScale = new Vector2(0, Mathf.Clamp(Random.Range(-0.002f, 0.002f), minVal, maxVal));
     }
 }
--- Assets/Scripts/Variety.cs
+++ Assets/Scripts/Variety.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class Variety : MonoBehaviour
 {
+    [SerializeField] float maxVal = 1.5f;
+    [SerializeField] float minVal = -1.5f;
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
             Destroy(other.gameObject);
             transform.localScale += new Vector3(0.5f, 0.5f, 0);
+            //transform.localScale = new Vector2(Mathf.Clamp(Random.Range(Vector3 ,minVal, maxVal)), 0);
+            //transform.localScale = new Vector2(0, Mathf.Clamp(Random.Range(-0.002f, 0.002f), minVal, maxVal));
         }
         if (other.gameObject.tag == "Allies")
         {
